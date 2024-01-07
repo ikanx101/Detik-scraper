e1b2dcaad10ef0ac784eb66c938d6a299b25d6a9
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2024-01-07 09:57:25 +0700

    Create Scraper.R

---
+++ Scraper.R
@@ -0,0 +1,25 @@
+# hapus environment
+rm(list=ls())
+
+# libraries
+library(dplyr)
+library(rvest)
+
+# url awal
+url = "https://www.detik.com/"
+
+# ambil headline
+headliner = 
+  url %>% 
+  read_html() %>% 
+  html_nodes(".media__link") %>% 
+  html_text(trim = T)
+headliner = headliner[headliner != ""]
+
+# buat nama file
+nama_file = Sys.time() %>% as.character()
+nama_file = paste0(nama_file,".csv")
+
+# save headliner
+write.csv(headliner,nama_file)
+# done
